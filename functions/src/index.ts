d2ae6fed98005f12e2cd58213e494b9e515657cb
Vladyslav Melnyk <v.v.melnyk1994@gmail.com>
2019-01-13 23:03:15 +0200

    add functions to work with firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,39 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp();
+let db = admin.firestore();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+export const getAllOffers = functions.https.onRequest((request, response) => {
+    var arr = [];
+    db.collection("offers").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            console.log(doc.id, " => ", doc.data());
+            arr.push(doc.data());
+        });
+        response.send(arr);
+    }).catch(function (error) {
+        console.error("Error showing all offers: ", error);
+    });
+});
 
-    console.log("Sent Hello World request!");
+export const addOffer = functions.https.onRequest((request, response) => {
+    let requestJson = request.body;
+    let offerObj = {
+        amount: requestJson.amount,
+        comment: requestJson.comment,
+        commission: requestJson.commission,
+        creation_time: requestJson.creation_time,
+        currency: requestJson.currency,
+        security_token: requestJson.security_token,
+        side: requestJson.side,
+        user: requestJson.user
+    };
+    db.collection("offers").add(offerObj)
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+    response.end();
 });
