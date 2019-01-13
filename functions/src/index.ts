7547f747af0b228ddaf066f2e83bff17e4213445
Maksym Tymchyk <reilag69@gmail.com>
2019-01-13 19:11:27 +0200

    Refactoring of 2 to 4 spaces / Add first log

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+
 admin.initializeApp();
 
-
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
+
+    console.log("Sent Hello World request!");
 });
